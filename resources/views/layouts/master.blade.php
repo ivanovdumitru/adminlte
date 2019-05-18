49e1dda1f437f5609712b7f902bdddebec8943fd
ivanovdumitru <ivanov.dumitru93@gmail.com>
2019-05-18 17:00:48 +0300

    Configure Vue Router to Laravel

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,12 +10,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>AdminLTE 3 | Starter</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<div class="wrapper" id="app">
 
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
@@ -44,7 +47,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <a href="index3.html" class="brand-link">
             <img src="./images/network.png" alt="Logo" class="brand-image img-circle elevation-3"
                  style="opacity: .8">
-            <span class="brand-text font-weight-light">Denis</span>
+            <span class="brand-text font-weight-light">AdminLte</span>
         </a>
 
         <!-- Sidebar -->
@@ -55,7 +58,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     <img src="./images/men.png" class="img-circle elevation-2" alt="Ivanov Denis">
                 </div>
                 <div class="info">
-                    <a href="#" class="d-block">Ivanov Denis</a>
+                    <a href="#" class="d-block">{{ Auth::user()->name}}</a>
                 </div>
             </div>
 
@@ -64,11 +67,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                     <!-- Add icons to the links using the .nav-icon class
                          with font-awesome or any other icon font library -->
+                    <li class="nav-item">
+                        <router-link to="/dashboard" class="nav-link">
+                            <i class="nav-icon fa fa-tachometer-alt"></i>
+                            <p>Dashboard</p>
+                        </router-link>
+                    </li>
                     <li class="nav-item has-treeview menu-open">
                         <a href="#" class="nav-link active">
-                            <i class="nav-icon fa fa-tachometer-alt"></i>
-                            <p>
-                                Starter Pages
+                            <i class="nav-icon fa fa-tasks"></i>
+                            <p>Management
                                 <i class="right fa fa-angle-left"></i>
                             </p>
                         </a>
@@ -85,16 +93,19 @@ scratch. This page gets rid of all links and provides the needed markup only.
                                     <p>Inactive Page</p>
                                 </a>
                             </li>
+                            <li class="nav-item">
+                                <router-link to="/profile" class="nav-link">
+                                    <i class="nav-icon fa fa-user"></i>
+                                    <p>Profile</p>
+                                </router-link>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="nav-icon fa fa-sign-out-alt"></i>
+                                    <p>Log Out</p>
+                                </a>
+                            </li>
                         </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon fa fa-th"></i>
-                            <p>
-                                Simple Link
-                                <span class="right badge badge-danger">New</span>
-                            </p>
-                        </a>
                     </li>
                 </ul>
             </nav>
@@ -126,64 +137,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <!-- Main content -->
         <div class="content">
             <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-
-                                <p class="card-text">
-                                    Some quick example text to build on the card title and make up the bulk of the card's
-                                    content.
-                                </p>
-
-                                <a href="#" class="card-link">Card link</a>
-                                <a href="#" class="card-link">Another link</a>
-                            </div>
-                        </div>
-
-                        <div class="card card-primary card-outline">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-
-                                <p class="card-text">
-                                    Some quick example text to build on the card title and make up the bulk of the card's
-                                    content.
-                                </p>
-                                <a href="#" class="card-link">Card link</a>
-                                <a href="#" class="card-link">Another link</a>
-                            </div>
-                        </div><!-- /.card -->
-                    </div>
-                    <!-- /.col-md-6 -->
-                    <div class="col-lg-6">
-                        <div class="card">
-                            <div class="card-header">
-                                <h5 class="m-0">Featured</h5>
-                            </div>
-                            <div class="card-body">
-                                <h6 class="card-title">Special title treatment</h6>
-
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-
-                        <div class="card card-primary card-outline">
-                            <div class="card-header">
-                                <h5 class="m-0">Featured</h5>
-                            </div>
-                            <div class="card-body">
-                                <h6 class="card-title">Special title treatment</h6>
-
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- /.col-md-6 -->
-                </div>
-                <!-- /.row -->
+               <router-view></router-view>
             </div><!-- /.container-fluid -->
         </div>
         <!-- /.content -->
